43abe3321f4071369731ca97dbe7013ea091d14e
Hatice Taşdemir <tdemirhtc@gmail.com>
2024-11-17 23:34:59 +0300

    Initial Commit

---
+++ JokesApi/JokesApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApiApp.swift
+//  JokesApi
+//
+//  Created by Hatice Taşdemir on 17.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct JokesApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
